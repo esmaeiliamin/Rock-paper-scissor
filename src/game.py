6d7c729173d095269e869bf6044c6824e1a7d5f5
esmaeiliamin <aminesmaeili439@gmail.com>
2025-04-29 13:28:29 +0330

    Add docstring

--- src/game.py
+++ src/game.py
@@ -2,11 +2,13 @@ import random
 
 
 class RockPaperScissors:
-    def __init__(self, name):
+    """Main class for Rock, Paper, Scissors game"""
+    def __init__(self, name: str):
         self.player_name = name
         self.choices = ["rock", "paper", "scissors"]
 
     def get_player_choice(self):
+        """Get the users choice"""
         user_choice = input(f'Enter your choice ({self.choices}):')
         if user_choice.lower() in self.choices:
             return user_choice.lower()
@@ -15,9 +17,19 @@ class RockPaperScissors:
             return self.get_player_choice()
 
     def get_computer_choice(self):
+        """Get computer choice randomly from the choices list"""
         return random.choice(self.choices)
 
     def determine_winner(self, user_choice, computer_choice):
+        """Decide the winner of the game based on the choices of the user and computer
+
+        Args:
+            user_choice (str): The choice of the user
+            computer_choice (str): The choice of the computer
+
+        Returns:
+            str: The winner of the game
+        """
         if user_choice == computer_choice:
             return "It's a Tie!"
         win_combinations = [("rock", "scissors"), ("scissors", "paper"), ("paper", "rock")]
@@ -27,6 +39,7 @@ class RockPaperScissors:
         return "Sorry! You lose!"
 
     def play(self):
+        """Play the game of Rock, Paper, Scissors"""
         user_choice = self.get_player_choice()
         computer_choice = self.get_computer_choice()
         winner = self.determine_winner(user_choice, computer_choice)
